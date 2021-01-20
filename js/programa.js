77daff71f2ff190b410165f9bc041e1c3299fcb5
SrPatito <69003176+SrPatito@users.noreply.github.com>
2021-01-19 18:20:58 -0600

    Add files via upload

---
+++ js/programa.js
@@ -0,0 +1,20 @@
+//Esto se utiliza para realizar una alerta de ventana
+//emergente.
+
+    //alert("Ya estas conectado a JavaScript")
+
+
+//VARIABLES CON TIPOS DE DATOS
+ var datoTexto = "Hola"
+ var datoNumerico = 1234
+ var datoDecimal = 3.1416
+ var datoChar = 'a'
+ var datosLogicos = true // o false
+ 
+  //UTILIZANDO UN ALERT PARA MOSTRAR LOS TIPOS DE DATOS
+  //CON VARIABLES
+ 
+ alert(datoTexto + " como estas " + datoNumerico)
+
+ alert(datoNumerico + datoDecimal)
+ 
